fffa054d15afa4c82f3fe5c149f648b2e65ad219
GP <gp@envoy.com>
2022-01-23 06:28:07 -0800

    gp/try-block-for-axios

--- dist/index.js
+++ dist/index.js
@@ -4773,37 +4773,41 @@ const core = __nccwpck_require__(6024);
 const axios = __nccwpck_require__(992);
 
 const main = async() => {
-    try {
-        const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+  try {
+    const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-        const url = core.getInput('url', {required: true});
-        const initDelayStr = core.getInput('init-delay', {required: true});
-        const retryCountStr = core.getInput('retry-count', {required: true});
-        const retryDelayStr = core.getInput('retry-delay', {required: true});
-        const codesAllowed = core.getInput('codes-allowed', {required: true});
+    const url = core.getInput('url', {required: true});
+    const initDelayStr = core.getInput('init-delay', {required: true});
+    const retryCountStr = core.getInput('retry-count', {required: true});
+    const retryDelayStr = core.getInput('retry-delay', {required: true});
+    const codesAllowed = core.getInput('codes-allowed', {required: true});
 
-        const initDelay = parseInt(initDelayStr) > 30000 ? 30000 : parseInt(initDelayStr);
-        var retryCount = parseInt(retryCountStr) > 5 ? 5 : parseInt(retryCountStr);
-        const retryDelay = parseInt(retryDelayStr) > 30000 ? 30000 : parseInt(retryDelayStr);
-        const codesAllowedArr = codesAllowed.split(',').map(Number);
+    const initDelay = parseInt(initDelayStr) > 30000 ? 30000 : parseInt(initDelayStr);
+    var retryCount = parseInt(retryCountStr) > 5 ? 5 : parseInt(retryCountStr);
+    const retryDelay = parseInt(retryDelayStr) > 30000 ? 30000 : parseInt(retryDelayStr);
+    const codesAllowedArr = codesAllowed.split(',').map(Number);
 
-        await sleep(initDelay);
-        var failed = true;
+    await sleep(initDelay);
+    var failed = true;
 
-        do {
-            await axios.get(url).then(response => {
-                failed = !codesAllowedArr.includes(response.status);
-            }).catch(function (error) {
-                console.log(`Error accessing url: ${error.message}`);
-            });
+    do {
+      try {
+        await axios.get(url).then(response => {
+          failed = !codesAllowedArr.includes(response.status);
+        }).catch(function (error) {
+          console.log(`Error accessing url: ${error.message}`);
+        });
+      } catch(error) {
+        console.log(`Error accessing url: ${error.message}`);
+      }
 
-            await sleep(retryDelay);
-        } while(retryCount-- > 0 && failed);
+      await sleep(retryDelay);
+    } while(retryCount-- > 0 && failed);
 
-        failed && core.setFailed('Failed to access url');
-    } catch (error) {
-        core.setFailed(error.message);
-    }
+    failed && core.setFailed('Failed to access url');
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 main();
